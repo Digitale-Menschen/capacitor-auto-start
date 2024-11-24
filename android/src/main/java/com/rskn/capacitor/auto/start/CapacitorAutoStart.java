065ee5b234ac27892b29beebd2a0ba75c506c876
Rosen Tsankov <cankovrosen@gmail.com>
2024-11-24 18:56:48 +0200

    add dynamic enable/disable of MY_PACKAGE_REPLACED and USER_PRESENT receivers

--- android/src/main/java/com/rskn/capacitor/auto/start/CapacitorAutoStart.java
+++ android/src/main/java/com/rskn/capacitor/auto/start/CapacitorAutoStart.java
@@ -15,29 +15,43 @@ public class CapacitorAutoStart {
     public static final String SERVICE_CLASS_NAME = "service";
 
     public void enableAutoStart(final Context context, final String className, boolean enabled, boolean isService, PluginCall call) {
-        int componentState;
-        SharedPreferences sp = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        if (enabled) {
-            componentState = PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
-            // Store the class name of your service or main activity for AppStarter
-            final String preferenceKey = isService ? SERVICE_CLASS_NAME : ACTIVITY_CLASS_NAME;
-            if (className != null) {
-                editor.putString(preferenceKey, className);
+            int componentState;
+            SharedPreferences sp = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            if (enabled) {
+                PackageManager pm = context.getPackageManager();
+
+                ComponentName bootCompletedReceiver = new ComponentName(context, BootCompletedReceiver.class);
+                ComponentName packageReplacedReceiver = new ComponentName(context, PackageReplacedReceiver.class);
+                ComponentName userPresentReceiver = new ComponentName(context, UserPresentReceiver.class);
+
+                pm.setComponentEnabledSetting(bootCompletedReceiver, enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                pm.setComponentEnabledSetting(
+                        packageReplacedReceiver,
+                        enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP
+                );
+                pm.setComponentEnabledSetting(
+                        userPresentReceiver,
+                        enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP
+                );
+
+
+                // Store the class name of your service or main activity for AppStarter
+                final String preferenceKey = isService ? SERVICE_CLASS_NAME : ACTIVITY_CLASS_NAME;
+                if (className != null) {
+                    editor.putString(preferenceKey, className);
+                } else {
+                    Log.e("CapacitorAutoStart", "No classname provided for enabling the plugin, seems getActivity() not working...");
+                    call.reject("No classname provided for enabling the plugin, seems getActivity() not working...");
+                }
             } else {
-                Log.e("CapacitorAutoStart", "No classname provided for enabling the plugin, seems getActivity() not working...");
-                call.reject("No classname provided for enabling the plugin, seems getActivity() not working...");
+                componentState = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+                editor.remove(ACTIVITY_CLASS_NAME);
+                editor.remove(SERVICE_CLASS_NAME);
             }
-        } else {
-            componentState = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-            editor.remove(ACTIVITY_CLASS_NAME);
-            editor.remove(SERVICE_CLASS_NAME);
+            editor.commit();
+            call.resolve();
         }
-        editor.commit();
-        // Enable or Disable BootCompletedReceiver
-        ComponentName bootCompletedReceiver = new ComponentName(context, BootCompletedReceiver.class);
-        PackageManager pm = context.getPackageManager();
-        pm.setComponentEnabledSetting(bootCompletedReceiver, componentState, PackageManager.DONT_KILL_APP);
-        call.resolve();
-    }
 }
